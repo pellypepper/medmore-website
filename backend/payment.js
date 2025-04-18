fa9658ec03e9e72afc908db6a15894fdf4b509fd
ppeliance <144333180+ppeliance@users.noreply.github.com>
2025-04-18 17:38:37 +0100

    g

--- backend/payment.js
+++ backend/payment.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY, {
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2020-08-27',
   appInfo: {
     name: "stripe-samples/checkout-one-time-payments",
--- check.js
+++ check.js
@@ -1,11 +1,11 @@
 const bcrypt = require('bcrypt');
-const pool = require('./db'); // Adjust this to your database connection
+const pool = require('./db'); 
 
 (async () => {
   try {
-    const adminEmail = 'popsyasnat@gmail.com'; // Replace with your admin email
-    const adminPassword = 'olayinkaA1'; // Replace with your desired default password
-    const hashedPassword = await bcrypt.hash(adminPassword, 10); // 10 is the salt rounds
+    const adminEmail = 'popsyasnat@gmail.com'; 
+    const adminPassword = 'olayinkaA1';
+    const hashedPassword = await bcrypt.hash(adminPassword, 10);
 
     // Corrected SQL query
    const response = await pool.query('INSERT INTO "users" (email, password) VALUES ($1, $2)',
